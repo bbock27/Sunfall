80bbe4c312c836abeaf73b094154e514f3a23bb3
Brady <bradybock11@icloud.com>
2024-11-09 18:42:08 -0500

    basic tilemap working with moveable character

---
+++ Assets/scripts/collision_controller.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public class HideTilemapColliderOnPlay : MonoBehaviour
+{
+
+    private TilemapRenderer tilemapRenderer;
+    public bool showCollision = false;
+    void Start()
+    {
+        tilemapRenderer = GetComponent<TilemapRenderer>();
+        tilemapRenderer.enabled = showCollision;
+    }
+    
+    void Update() {
+    	tilemapRenderer.enabled = showCollision;
+    }
+}
